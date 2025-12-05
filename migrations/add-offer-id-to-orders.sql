2609c08586b0922c8893cb2ecbb02e603541ef34
monikajha100 <monikailasharma@gmail.com>
2025-12-05 15:07:27 +0530

    Delete migrations directory

--- migrations/add-offer-id-to-orders.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Add offer_id column to orders table
--- Run this SQL in your MySQL database
-
-ALTER TABLE `orders` 
-ADD COLUMN `offer_id` INT(11) DEFAULT NULL COMMENT 'Special offer ID if this order used a special offer' AFTER `razorpay_order_id`,
-ADD KEY `idx_offer_id` (`offer_id`),
-ADD CONSTRAINT `orders_ibfk_offer` FOREIGN KEY (`offer_id`) REFERENCES `special_offers` (`id`) ON DELETE SET NULL;
-
